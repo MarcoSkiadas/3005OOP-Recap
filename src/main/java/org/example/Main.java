a509c23409bc55e91264e210b1e10ac2c5e81137
MarcoSkiadas <Skiadas2@hotmail.com>
2024-05-30 14:34:20 +0200

    Adding Books in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Book books1 = new Book("Test","mcTest","1234");
+        Book books2 = new Book("Test2","mcTest2","1233");
+        Book books3 = new Book("Test3","mcTest3","1232");
+
+        System.out.println(books1+"\n"+books2+"\n"+books3+"\n");
+
+
     }
 }
